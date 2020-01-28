b962113d563f699265c0c545069cf2a72e5d18fd
Jose Luis Perdomo Andrade <Programador.Junior4@interedes.com.co>
2020-01-28 14:06:07 -0500

    Avances

---
+++ output/MaterialRetiradoIR.model.dart
@@ -0,0 +1,40 @@
+class MaterialRetiradoModelIR with ModsimORM implements ModsimModel { 
+  String nombre;  int estado;  int id;  int idRemoto;  bool imagenesSync;  bool guardadoLocal;  MaterialRetiradoModelIR({    this.nombre,    this.estado,    this.id,    this.idRemoto,    this.imagenesSync,    this.guardadoLocal,  });
+            
+  @override
+  int employeeId;
+
+  @override
+  bool hasChanges;
+
+  @override
+  DateTime movilToWeb;
+
+  @override
+  DateTime webToMovil;
+
+
+  @override
+  MaterialRetiradoModelIR fromJSON(Map<String, dynamic> json) => MaterialRetiradoModelIR(
+    nombre: json["nombre"],    estado: json["estado"],    id: json["id"],    idRemoto: json["idRemoto"],    imagenesSync: json["imagenesSync"],    guardadoLocal: json["guardadoLocal"],  );
+  @override
+  Map<String, dynamic> toJSON() => {
+    "nombre": nombre,    "estado": estado,    "id": id,    "idRemoto": idRemoto,    "imagenesSync": imagenesSync,    "guardadoLocal": guardadoLocal,  };
+  @override
+  MaterialRetiradoModelIR fromSqlite(Map<String, dynamic> json) => MaterialRetiradoModelIR(
+    nombre: json["nombre"] ,    estado: json["estado"] ,    id: json["id"] ,    idRemoto: json["idRemoto"] ,    imagenesSync: json["imagenesSync"]  = (json["imagenesSync"]  == null || json["imagenesSync"] == 0) ? false : true,    guardadoLocal: json["guardadoLocal"]  = (json["guardadoLocal"]  == null || json["guardadoLocal"] == 0) ? false : true,  );
+  @override
+  Map<String, dynamic> toSqlite() => {
+    "nombre": nombre,    "estado": estado,    "id": id,    "idRemoto": idRemoto,    "imagenesSync": imagenesSync == true ? 1 : 0,    "guardadoLocal": guardadoLocal == true ? 1 : 0,  };
+  save() async{
+    await initDatabase();
+    ResponseDB r;
+    if(this.id == 0 || this.id == null){
+        this.id = null;
+        r = await add('MaterialRetirado', this);
+    } else {
+        r = await update('MaterialRetirado', this);
+    }
+    return r;
+  }
+}
--- src/read.py
+++ src/read.py
@@ -6,5 +6,5 @@ class Reader:
     data = {}
 
     def __init__(self):
-        with open(self.path + 'models/ReconexionIR.json') as f:
+        with open(self.path + 'models/MaterialRetiradoIR.json') as f:
             self.data = json.load(f)
