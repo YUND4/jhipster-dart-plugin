4300c2acc1de688bbdc9e3c93e3f02331903a76d
Jose Luis Perdomo Andrade <Programador.Junior4@interedes.com.co>
2020-01-28 09:57:56 -0500

    Validaciones de tipo

--- output/Job.model.dart
+++ output/Job.model.dart
@@ -1,43 +1,17 @@
 class JobModel with ModsimORM implements ModsimModel { 
-
-  String jobTitle;
-  int minSalary;
-  int maxSalary;
-
-  JobModel({
-    this.jobTitle,
-    this.minSalary,
-    this.maxSalary,
-  });
-
+  String jobTitle;  int minSalary;  int maxSalary;  DateTime date;  bool exist;  JobModel({    this.jobTitle,    this.minSalary,    this.maxSalary,    this.date,    this.exist,  });
   @override
   JobModel fromJSON(Map<String, dynamic> json) => JobModel(
-    jobTitle: json[jobTitle],
-    minSalary: json[minSalary],
-    maxSalary: json[maxSalary],
-  );
-
+    jobTitle: json["jobTitle"],    minSalary: json["minSalary"],    maxSalary: json["maxSalary"],    date: json["date"],    exist: json["exist"],  );
   @override
   Map<String, dynamic> toJSON() => {
-    "jobTitle": jobTitle,
-    "minSalary": minSalary,
-    "maxSalary": maxSalary,
-  };
-
+    "jobTitle": jobTitle,    "minSalary": minSalary,    "maxSalary": maxSalary,    "date": date is DateTime ? "${date.year.toString().padLeft(4, '0')}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}T${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}:${date.second.toString().padLeft(2, '0')}-05:00" : null,    "exist": exist,  };
   @override
   JobModel fromSqlite(Map<String, dynamic> json) => JobModel(
-    jobTitle: json[jobTitle],
-    minSalary: json[minSalary],
-    maxSalary: json[maxSalary],
-  );
-
+    jobTitle: json["jobTitle"] ,    minSalary: json["minSalary"] ,    maxSalary: json["maxSalary"] ,    date: json["date"]  is String ? DateTime.parse(json["date"]) : json["date"],    exist: json["exist"]  = (json["exist"]  == null || json["exist"] == 0) ? false : true,  );
   @override
   Map<String, dynamic> toSqlite() => {
-    "jobTitle": jobTitle,
-    "minSalary": minSalary,
-    "maxSalary": maxSalary,
-  };
-
+    "jobTitle": jobTitle,    "minSalary": minSalary,    "maxSalary": maxSalary,    "date": date is DateTime ? "${date.year.toString().padLeft(4, '0')}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}T${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}:${date.second.toString().padLeft(2, '0')}-05:00" : null,    "exist": exist == true ? 1 : 0,  };
   save() async{
     await initDatabase();
     ResponseDB r;
--- src/model.py
+++ src/model.py
@@ -17,16 +17,29 @@ class Model:
             result = t
         return result
 
-    def validator (self, t):
+    def validator (self, t, typo, tosqlite = False, fromsqlite = False):
         result = t
+        if (typo == "DateTime") :
+            result = t + ' is ' + typo + ' ? "${' + t + '.year.toString().padLeft(4, ' + "'0" + "'" + ')}-${' + t + '.month.toString().padLeft(2, ' + "'0" + "'" + ')}-${' + t + '.day.toString().padLeft(2, ' + "'0" + "'" + ')}T${' + t + '.hour.toString().padLeft(2, ' + "'0" + "'" + ')}:${' + t + '.minute.toString().padLeft(2, ' + "'0" + "'" + ')}:${' + t + '.second.toString().padLeft(2, ' + "'0" + "'" + ')}-05:00" : null'
+        
+        if (typo == "bool" and tosqlite):
+            result = t + ' == true ? 1 : 0'
+
+        if (fromsqlite) :
+            result = ""
+        if (typo == "DateTime" and fromsqlite):
+            result = ' is String ? DateTime.parse(json["' + t + '"]) : json["' + t + '"]'
+        if (typo == "bool" and fromsqlite):
+            result = ' = (json["' + t + '"]  == null || json["' + t + '"] == 0) ? false : true'
+        
         return result
 
     def generator(self):
         self.modelName = self.data['name'] + 'Model'
         fields =  self.data['fields']
-        if not os.path.exists(self.path + 'output\\'):
-            os.makedirs(self.path + 'output\\')
-        with open(self.path + 'output\\' + self.data.get('name', '') + '.model.dart', 'w+') as f:
+        if not os.path.exists(self.path + 'output/'):
+            os.makedirs(self.path + 'output/')
+        with open(self.path + 'output/' + self.data.get('name', '') + '.model.dart', 'w+') as f:
             f.write('class ' + self.modelName + ' with ModsimORM implements ModsimModel { \r\n')
             for i in fields:
                 f.write('  ' + self.typed(i['fieldType']) + ' ' + i['fieldName'] + ';\r')
@@ -39,28 +52,28 @@ class Model:
   %s fromJSON(Map<String, dynamic> json) => %s(
 ''' % (self.modelName, self.modelName))
             for i in fields:
-                f.write('    %s: json[%s],\r' % (i['fieldName'], i['fieldName']))
+                f.write('    %s: json["%s"],\r' % (i['fieldName'], i['fieldName']))
             f.write('  );\r')
             f.write('''
   @override
   Map<String, dynamic> toJSON() => {
 ''')
             for i in fields:
-                f.write('    "%s": %s,\r' % (i['fieldName'], self.validator(i['fieldName'])))
+                f.write('    "%s": %s,\r' % (i['fieldName'], self.validator(i['fieldName'], i['fieldType'])))
             f.write('  };\r')
             f.write('''
   @override
   %s fromSqlite(Map<String, dynamic> json) => %s(
 ''' % (self.modelName, self.modelName))
             for i in fields:
-                f.write('    %s: json[%s],\r' % (i['fieldName'], i['fieldName']))
+                f.write('    %s: json["%s"] %s,\r' % (i['fieldName'], i['fieldName'], self.validator(i['fieldName'], i['fieldType'], False, True)))
             f.write('  );\r')
             f.write('''
   @override
   Map<String, dynamic> toSqlite() => {
 ''')
             for i in fields:
-                f.write('    "%s": %s,\r' % (i['fieldName'], self.validator(i['fieldName'])))
+                f.write('    "%s": %s,\r' % (i['fieldName'], self.validator(i['fieldName'], i['fieldType'], True)))
             f.write('  };\r')
             f.write('''
   save() async{
