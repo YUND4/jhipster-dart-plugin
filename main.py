23224c6e11f01213343a204ad3128c4022a139f0
Enuar.Munoz <enuar.munoz@interedes.com.co>
2020-01-28 10:12:53 -0500

    changes migrations

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from src.read import Reader
 from src.model import Model
+from src.migration import Migration
+
 
 if __name__ == "__main__":
     r = Reader()
     Model(r.data, r.path)
+    Migration(r.data, r.path)
--- src/migration.py
+++ src/migration.py
@@ -1,15 +1,46 @@
-class migration:
+import os
+
+
+class Migration:
     path = ''
     data = {}
+    modelName = ''
 
     def __init__(self, r, p):
         self.data = r
         self.path = p
         self.generator()
 
+    def typed (self, t):
+        if (t =='Long'):
+            result = 'INTEGER'
+        elif  (t =='String'):
+            result = 'TEXT'
+        elif  (t =='String'):
+            result = 'TEXT'
+        elif  (t =='Instant'):
+            result = 'TIMESTAMP'
+        else:
+            result = t
+        return result
+
     def generator(self):
-        if not os.path.exists(self.path + 'output\\'):
-            os.makedirs(self.path + 'output\\')
-        with open(self.path + 'output\\' + self.data.get('name', '') + '.sql', 'w+') as f:
-            for i in range(10):
-                f.write("This is line %d\r\n" % (i+1))
+        self.modelName = self.data['name']
+        fields =  self.data['fields']
+        if not os.path.exists(self.path + 'output/migrations'):
+            os.makedirs(self.path + 'output/migrations')
+        
+        with open(self.path + 'output/migrations\\' + self.data.get('name', '') + '.sql', 'w+') as f:
+            
+            f.write('CREATE TABLE ' + self.modelName + ' ( \n')
+            for i in fields:
+                f.write('    '  + i['fieldName'] +' ' + self.typed(i['fieldType']) + ' ' + ',\r')
+
+            """ SET PROPERTIES STATIC  """
+            f.write('   id  INTEGER '          + ',\r')
+            f.write('   idRemoto  INTEGER'     + ',\r')
+            f.write('   imagenesSync BOOLEAN'  + ',\r')
+
+            """ END PROPERTIES STATIC  """
+            
+            f.write('  );\r' )
