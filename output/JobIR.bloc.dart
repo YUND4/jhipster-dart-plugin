13a4f57938679b511af6e3db4eb1e1f1438d2934
syunda <programador.junior1@interedes.com.co>
2020-01-28 10:58:21 -0500

    Changes

--- output/JobIR.bloc.dart
+++ output/JobIR.bloc.dart
@@ -1,39 +1,6 @@
 class JobBlocIR with Validators implements BlocBase { 
   final listenConection = ConnectionStatusSingleton.getInstance();
-  final _jobListController = BehaviorSubject<JobModelIR>();
-  final _idController = BehaviorSubject<int>();
-  final _jobTitleController = BehaviorSubject<String>;
-  final _minSalaryController = BehaviorSubject<int>;
-  final _maxSalaryController = BehaviorSubject<int>;
-  final _dateController = BehaviorSubject<DateTime>;
-  final _existController = BehaviorSubject<bool>;
-
-  Stream<int> get idStream => _idController.stream;
-  Stream<String> get jobTitleStream => _jobTitleController.stream;
-  Stream<int> get minSalaryStream => _minSalaryController.stream;
-  Stream<int> get maxSalaryStream => _maxSalaryController.stream;
-  Stream<DateTime> get dateStream => _dateController.stream;
-  Stream<bool> get existStream => _existController.stream;
-  Stream<dynamic> get formValidStream => Observable.combineLatest([
-    jobTitleStream,
-    minSalaryStream,
-    maxSalaryStream,
-    dateStream,
-    existStream
-  ], (a) => a);
-
-  Function(JobModelIR) get reconexionSink => _jobListController.sink.add;
-  Function(String) get jobTitleSink => _jobTitleController.sink.add;
-  Function(int) get minSalarySink => _minSalaryController.sink.add;
-  Function(int) get maxSalarySink => _maxSalaryController.sink.add;
-  Function(DateTime) get dateSink => _dateController.sink.add;
-  Function(bool) get existSink => _existController.sink.add;
-
-  JobModelIR get reconexionModeloValue => _jobListController.value;
-  int get idValue => _idController.value;
-  String get jobTitleValue => _jobTitleController.value;
-  int get minSalaryValue => _minSalaryController.value;
-  int get maxSalaryValue => _maxSalaryController.value;
-  DateTime get dateValue => _dateController.value;
-  bool get existValue => _existController.value;
-
+  final _jobListController = BehaviorSubject<JobModelIR>();  final _idController = BehaviorSubject<int>();
+  final _idRemotoController = BehaviorSubject<int>();
+  final _jobTitleController = BehaviorSubject<String>;  final _minSalaryController = BehaviorSubject<int>;  final _maxSalaryController = BehaviorSubject<int>;  final _dateController = BehaviorSubject<DateTime>;  final _existController = BehaviorSubject<bool>;  Stream<int> get idStream => _idController.stream;  Stream<String> get jobTitleStream => _jobTitleController.stream;  Stream<int> get minSalaryStream => _minSalaryController.stream;  Stream<int> get maxSalaryStream => _maxSalaryController.stream;  Stream<DateTime> get dateStream => _dateController.stream;  Stream<bool> get existStream => _existController.stream;  Stream<dynamic> get formValidStream => Observable.combineLatest([    jobTitleStream,    minSalaryStream,    maxSalaryStream,    dateStream,    existStream  ], (a) => a);
+  Function(JobModelIR) get reconexionSink => _jobListController.sink.add;  Function(int) get idSink => _idController.sink.add;  Function(int) get idRemotoSink => _idRemotoController.sink.add;  Function(String) get jobTitleSink => _jobTitleController.sink.add;  Function(int) get minSalarySink => _minSalaryController.sink.add;  Function(int) get maxSalarySink => _maxSalaryController.sink.add;  Function(DateTime) get dateSink => _dateController.sink.add;  Function(bool) get existSink => _existController.sink.add;  JobModelIR get JobModelIRValue => _jobListController.value;  int get idValue => _idController.value;  String get jobTitleValue => _jobTitleController.value;  int get minSalaryValue => _minSalaryController.value;  int get maxSalaryValue => _maxSalaryController.value;  DateTime get dateValue => _dateController.value;  bool get existValue => _existController.value;  void setModel(JobModelIR model) {    idSink(model.id);    idRemotoSink(model.idRemoto);    jobTitleSink(model.jobTitle);    minSalarySink(model.minSalary);    maxSalarySink(model.maxSalary);    dateSink(model.date);    existSink(model.exist);  }  JobModelIR getModel() {    return JobModelIR(      jobTitle: jobTitleValue,      minSalary: minSalaryValue,      maxSalary: maxSalaryValue,      date: dateValue,      exist: existValue    );  }  dispose() {    _jobTitleController?.close();    _minSalaryController?.close();    _maxSalaryController?.close();    _dateController?.close();    _existController?.close();  }}
--- src/bloc.py
+++ src/bloc.py
@@ -37,6 +37,7 @@ class Bloc:
             f.write('  final listenConection = ConnectionStatusSingleton.getInstance();\r\n')
             f.write('  final _' + self.data['name'].lower() + 'ListController = BehaviorSubject<' + self.modelName + '>();\r')
             f.write('  final _idController = BehaviorSubject<int>();\r\n')
+            f.write('  final _idRemotoController = BehaviorSubject<int>();\r\n')
             for i in fields:
                 f.write('  final _' + i['fieldName'] + 'Controller = BehaviorSubject<' + self.typed(i['fieldType']) + '>;\r')
             f.write('\r')
@@ -52,11 +53,36 @@ class Bloc:
             f.write('  ], (a) => a);\r\n')
             f.write('\r')
             f.write('  Function(' + self.modelName + ') get reconexionSink => _' + self.data['name'].lower() + 'ListController.sink.add;\r')
+            f.write('  Function(int) get idSink => _idController.sink.add;\r')
+            f.write('  Function(int) get idRemotoSink => _idRemotoController.sink.add;\r')
             for i in fields:
                 f.write('  Function(' + self.typed(i['fieldType']) + ') get ' + i['fieldName'] + 'Sink => _' + i['fieldName'] + 'Controller.sink.add;\r')
             f.write('\r')
-            f.write('  ' + self.modelName +' get reconexionModeloValue => _' + self.data['name'].lower() + 'ListController.value;\r')
+            f.write('  ' + self.modelName +' get ' + self.modelName +'Value => _' + self.data['name'].lower() + 'ListController.value;\r')
             f.write('  int get idValue => _idController.value;\r')
             for i in fields:
                 f.write('  ' + self.typed(i['fieldType']) + ' get ' + i['fieldName'] + 'Value => _' + i['fieldName'] + 'Controller.value;\r')
             f.write('\r')
+            f.write('  void setModel(' + self.modelName +' model) {\r')
+            f.write('    idSink(model.id);\r')
+            f.write('    idRemotoSink(model.idRemoto);\r')
+            for i in fields:
+                f.write('    '  + i['fieldName'] + 'Sink(model.' + i['fieldName'] + ');\r')
+            f.write('  }\r')
+            f.write('\r')
+            f.write('  ' + self.modelName +' getModel() {\r')
+            f.write('    return ' + self.modelName +'(\r')
+            for i in fields:
+                if fields[-1] == i:
+                    f.write('      ' + i['fieldName'] + ': ' + i['fieldName'] + 'Value\r')
+                else:
+                    f.write('      ' + i['fieldName'] + ': ' + i['fieldName'] + 'Value,\r')
+            f.write('    );\r')
+            f.write('  }\r')
+            f.write('\r')
+            f.write('  dispose() {\r')
+            for i in fields:
+                f.write('    _'  + i['fieldName'] + 'Controller?.close();\r')
+            f.write('  }\r')
+            f.write('}\r')
+            
